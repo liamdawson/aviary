0ca973f2d613263ff7f805bd26fd21a48e9f0c86
Liam Dawson <liam@ldaws.com>
2019-07-21 12:23:51 +1000

    add hyper tab icons

--- src/files/gui_dev_tools/.hyper.js
+++ src/files/gui_dev_tools/.hyper.js
@@ -146,7 +146,7 @@ module.exports = {
     //   `hyperpower`
     //   `@company/project`
     //   `project#1.0.1`
-    plugins: ["hyper-dracula", "hypercwd"],
+    plugins: ["hyper-dracula", "hypercwd", "hyper-tab-icons"],
   
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
