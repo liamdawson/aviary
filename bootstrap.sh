de6f997b70ca546445e22969331fea3416e1eb30
Liam Dawson <liam@ldaws.com>
2019-08-23 22:10:57 +1000

    start supporting apt

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,15 @@
 
 set -eu
 
-if command -v dnf >/dev/null
-then
+is_command() {
+  command -v "$1" >/dev/null
+}
+
+if is_command apt-get; then
+  sudo apt-get install -y git python3-pip
+  is_command ansible || pip3 install --user ansible
+elif is_command dnf; then
   sudo dnf install -y git python3-pip
-  pip3 install --user ansible
+  is_command ansible || pip3 install --user ansible
 fi
+
