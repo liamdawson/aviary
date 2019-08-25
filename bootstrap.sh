a81cef22e05459b2f0d9bb1e0ee8f1daa2ef839e
Liam Dawson <liam@ldaws.com>
2019-08-25 14:15:59 +1000

    try bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,3 +14,15 @@ elif is_command dnf; then
   is_command ansible || pip3 install --user ansible
 fi
 
+if ! [ -f ~/.ssh/id_ed25519 ]; then
+  ssh-keygen -t ed25519
+  echo "Add the following as a deploy key (with write access)"
+  cat ~/.ssh/id_ed25519.pub
+  echo "at https://github.com/liamdawson/aviary/settings/keys"
+fi
+
+if ! [ -d ~/.aviary ]; then
+  git clone git@github.com:/liamdawson/aviary.git ~/.aviary \
+    --depth=1 \
+    -c "user.email=liam@ldaws.com"
+fi
